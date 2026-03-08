137057a962a0747cbfa649d7a6e33daa3db11b68
ucEzette <u.ezette@alustudent.com>
2026-03-08 18:10:51 +0200

    Removed `output: 'standalone'` from `next.config.js` and deleted `vercel.json`.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,5 @@ const nextConfig = {
     typescript: { ignoreBuildErrors: true },
     eslint: { ignoreDuringBuilds: true },
     reactStrictMode: true,
-    output: 'standalone',
 };
 module.exports = nextConfig;
