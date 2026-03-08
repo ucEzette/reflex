6f3397ad7ec30748775ffb6db91f4978df40bf12
ucEzette <u.ezette@alustudent.com>
2026-03-08 19:24:17 +0200

    feat: configure Next.js to output a standalone build.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,5 +3,6 @@ const nextConfig = {
     typescript: { ignoreBuildErrors: true },
     eslint: { ignoreDuringBuilds: true },
     reactStrictMode: true,
+    output: 'standalone',
 };
 module.exports = nextConfig;
