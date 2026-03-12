5425e2cd7dc860f8a3f25a7253f1400f9ca6e2f9
ucEzette <u.ezette@alustudent.com>
2026-03-12 23:52:36 +0200

    feat: Update frontend documentation links to an intro deck and migrate the AI agent from Google Generative AI to OpenClaw.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -77,10 +77,10 @@ export default function Home() {
                   <span className="dexter-btn-drawer dexter-transition-bottom whitespace-nowrap !text-[11px]">mainnet active</span>
                 </button>
               </a>
-              <a href="/docs" className="dexter-btn-container w-52 relative z-30" style={{ '--btn-color': '#475569' } as React.CSSProperties}>
+              <a href="https://docs.google.com/presentation/d/1RAQHRFVr9NHClst8C7FdOXr4fHUsxcsug20wHwEC8Ak/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="dexter-btn-container w-52 relative z-30" style={{ '--btn-color': '#475569' } as React.CSSProperties}>
                 <button className="dexter-btn !min-w-[200px] !min-h-[50px] !px-6 !py-3 !rounded-xl opacity-90 hover:opacity-100" type="button">
                   <span className="dexter-btn-drawer dexter-transition-top !text-[11px]">DOCS</span>
-                  <span className="dexter-btn-text flex items-center gap-2 !text-lg !text-foreground/90"><span className="material-symbols-outlined text-[20px]">description</span> Whitepaper</span>
+                  <span className="dexter-btn-text flex items-center gap-2 !text-lg !text-foreground/90"><span className="material-symbols-outlined text-[20px]">description</span> Our Intro Deck</span>
                   <svg className="dexter-btn-corner !w-[40px]" viewBox="0 0 100 100"><path d="M 0 0 L 100 0 L 100 100 L 98 100 L 98 2 L 0 2 Z"></path></svg>
                   <svg className="dexter-btn-corner !w-[40px]" viewBox="0 0 100 100"><path d="M 0 0 L 100 0 L 100 100 L 98 100 L 98 2 L 0 2 Z"></path></svg>
                   <svg className="dexter-btn-corner !w-[40px]" viewBox="0 0 100 100"><path d="M 0 0 L 100 0 L 100 100 L 98 100 L 98 2 L 0 2 Z"></path></svg>
@@ -306,10 +306,10 @@ export default function Home() {
                   <span className="dexter-btn-drawer dexter-transition-bottom whitespace-nowrap !text-[10px]">mainnet</span>
                 </button>
               </a>
-              <a href="/docs" className="dexter-btn-container w-48 relative z-30" style={{ '--btn-color': '#475569' } as React.CSSProperties}>
+              <a href="https://docs.google.com/presentation/d/1RAQHRFVr9NHClst8C7FdOXr4fHUsxcsug20wHwEC8Ak/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="dexter-btn-container w-48 relative z-30" style={{ '--btn-color': '#475569' } as React.CSSProperties}>
                 <button className="dexter-btn !min-w-[180px] !min-h-[44px] !px-4 !py-2 opacity-90 hover:opacity-100" type="button">
                   <span className="dexter-btn-drawer dexter-transition-top">DOCS</span>
-                  <span className="dexter-btn-text flex items-center gap-2 !text-base !text-foreground/90">Documentation</span>
+                  <span className="dexter-btn-text flex items-center gap-2 !text-base !text-foreground/90">Our Intro Deck</span>
                   <svg className="dexter-btn-corner" viewBox="0 0 100 100"><path d="M 0 0 L 100 0 L 100 100 L 98 100 L 98 2 L 0 2 Z"></path></svg>
                   <svg className="dexter-btn-corner" viewBox="0 0 100 100"><path d="M 0 0 L 100 0 L 100 100 L 98 100 L 98 2 L 0 2 Z"></path></svg>
                   <svg className="dexter-btn-corner" viewBox="0 0 100 100"><path d="M 0 0 L 100 0 L 100 100 L 98 100 L 98 2 L 0 2 Z"></path></svg>
